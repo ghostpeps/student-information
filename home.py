00141f80034b00bc59b820dd4315aa2ebe64c78d
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-08-07 19:30:01 -0700

    Update home.py

--- home.py
+++ home.py
@@ -4,6 +4,14 @@ import streamlit as st
 
 def home(is_logged_in):
     if is_logged_in is True:
-        pass
+        st.title("Home")
+        st.write("Welcome to your account.")
     elif is_logged_in is False:
-        pass
+        st.title("Please login in the settings tab first.")
+
+pages = [
+    st.Page(page=home(is_logged_in), title="Home", icon=":material/home:"),
+    st.Page(page="settings.py", title="Settings", icon=":material/settings:")
+]
+pgs = st.navigation(pages)
+pgs.run()
