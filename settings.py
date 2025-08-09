ecdfa82412ec658be87b34afaf8c6fb613bc55cb
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-08-09 12:16:54 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,16 @@
 import streamlit as st
 
-@st.dialog("Are you sure?")
+@st.dialog(title="Are you sure?", dismissible=False)
 def delete_account():
-  st.write("")
+  st.write("By selecting Yes, your account will be permanently deleted.")
+  col1, col2 = st.columns(2)
+  no = col1.button(label="No", icon=":material/close:")
+  yes = col2.button(label="Yes, and I understand the consequences.", type="primary", icon=":material/delete_forever:")
+  if no:
+    st.rerun()
+  if yes:
+    global p
+    p = st.text_input(label=" ", type="password", label_visibility="collapsed", placeholder="e.g. password#123", icon=":material/password:")
 try:
   if is_logged_in is True:
     pass
@@ -17,7 +25,13 @@ finally:
       is_logged_in = False
       st.rerun()
     if delete:
-      
+      delete_account()
+      with open("passwords.txt", r) as f:
+        try:
+          if p not in f.readlines:
+            pass
+        except NameError:
+          
   elif is_logged_in is False:
     st.title("Please login or sign up")
     login = st.button(label="Login", icon=":material/login:")
